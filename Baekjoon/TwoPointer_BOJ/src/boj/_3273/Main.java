854f7d9e576585094673050f2e54f9b035793e40
YuJin <lyubliss@gmail.com>
2021-07-22 00:51:07 +0900

    Two Pointer : 3273

---
+++ Baekjoon/TwoPointer_BOJ/src/boj/_3273/Main.java
@@ -0,0 +1,54 @@
+package boj._3273;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * Two Pointer
+ * 백준 3273번 풀이
+ * 
+ * Coded by NANAEU	(2021.07.15)
+ */
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int start = 0, end = 1, cnt = 0;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		for(int i=0 ; i<n ; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		int x = Integer.parseInt(br.readLine());
+		
+		while(true) {
+			
+			if(arr[start]+arr[end] == x) cnt++;
+			
+			if(start == n-2) break;
+			
+			if(end < n-1) {
+				end++;
+			}
+			else if(end == n-1){
+				start++;
+				end = start+1;
+			}
+		}
+
+		
+		System.out.println(cnt);
+	}
+
+}
