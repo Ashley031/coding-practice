5b2559e847fd728445b4c728c63893137f7df901
YuJin <lyuashley@gmail.com>
2021-12-06 18:20:43 +0900

    BOJ 7576 참고 링크

--- Baekjoon/DFSBFS/src/BOJ7576/Main.java
+++ Baekjoon/DFSBFS/src/BOJ7576/Main.java
@@ -7,6 +7,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+* 참고: https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-7576%EB%B2%88-%ED%86%A0%EB%A7%88%ED%86%A0-%EC%9E%90%EB%B0%94Java
+* */
+
 class Tomato {
     int x;
     int y;
