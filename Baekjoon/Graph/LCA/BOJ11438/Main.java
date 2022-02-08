cdc3019e733f1a0a6397447753c698bcdcba6b9c
Yujin <lyubliss@gmail.com>
2022-02-08 16:17:19 +0900

    edit: BOJ 11438 주석 추가

--- Baekjoon/Graph/LCA/BOJ11438/Main.java
+++ Baekjoon/Graph/LCA/BOJ11438/Main.java
@@ -50,13 +50,14 @@ public class Main {
         bw.close();
         br.close();
     }
+    
     static void getLogN() {
         logN = 1;
         for(int i=1 ; i<=N ; i*=2)
             logN++;
     }
 
-    // Init depth
+    // Init depth & parents[0]
     static void doBfs(int start) {
         ArrayDeque<Integer> dq = new ArrayDeque<>();
         depth[start] = 1;
@@ -77,24 +78,29 @@ public class Main {
     static void makeSparseTable() {
         for(int i=1 ; i<=logN ; i++){
             for(int j=1 ; j<=N ; j++){
+                // LCA 점화식
                 parents[i][j] = parents[i-1][parents[i-1][j]];
             }
         }
     }
 
     static int getLCA(int a, int b) {
+        // 깊이가 깊은 것부터 조작할 수 있게 조정
         if(depth[a] < depth[b])
             return getLCA(b,a);
 
+        // b와 깊이가 맞을 때까지 a를 올린다
         for(int i=0 ; i<=logN ; i++){
             if(((depth[a] - depth[b]) & (1<<i)) >= 1){
                 a = parents[i][a];
             }
         }
 
+        // 높이를 맞췄다면 같은지 검사
         if(a == b)
             return a;
 
+        // 두 부모가 같을 때까지 올라감
         for(int i=logN ; i>=0 ; i--){
             if(parents[i][a] != parents[i][b]){
                 a = parents[i][a];
@@ -102,6 +108,7 @@ public class Main {
             }
         }
 
+        // a와 b 둘 중 어느 부모를 리턴해도 어차피 같은 LCA
         return parents[0][a];
     }
 }
